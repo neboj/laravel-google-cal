939589af712025ae1ea0f308de9c273ae2e03dc7
nebozoid <nebojsa.kozlovacki@gmail.com>
2021-07-01 17:35:12 +0200

    Remove unused routes

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -17,26 +17,7 @@ class HomepageController extends Controller
          } else {
              return $results->getHtmlLink();
          }
-//        $results = $OAuthService->createEvent($request);
-//        if (empty($results)) {
-//            return ['Failed in php'];
-//        } else {
-//            var_dump($results->getHtmlLink());
-//        }
-
-        return view('welcome');
-    }
-
-    function ajax(Request  $request, OAuthService $OAuthService) {
-        $data = urldecode($request->getQueryString());
-        $data = substr_replace($data, "}", -1);
-        $data = json_decode($data);
-        $results = $OAuthService->createEvent($data);
-        if (empty($results)) {
-            return ['Api failed'];
-        } else {
-            return $results->getHtmlLink();
-        }
+//        return view('welcome');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['cors'])->group(function () {
     Route::get('/', [\App\Http\Controllers\HomepageController::class, 'index'])->name('home.index');
 });
-Route::get('/ajax', [\App\Http\Controllers\HomepageController::class, 'ajax'])->name('home.ajax');
